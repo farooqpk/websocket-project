220568b4d5579f58545de876de1b8b0c312a80a4
Ummar Farooq <farooqcherur@gmail.com>
2023-06-23 12:12:12 +0530

    modified createFrame.js by adding better comments

--- src/utils/frameUtils/createFrame.js
+++ src/utils/frameUtils/createFrame.js
@@ -1,39 +1,36 @@
-
 module.exports.createFrame = (data) => {
-
   const payload = JSON.stringify(data);
-  
+
   // Calculate the length of the payload in bytes
   const payloadByteLength = Buffer.byteLength(payload);
- 
-  // payload offset means the positon of the payload in the buffer frame
+
+  // payload offset means the starting positon of the payload in the buffer frame
   let payloadBytesOffset = 2;
 
-  // Check if the payload length exceeds 65535 bytes
-  if (payloadByteLength > 65535) {
-    // The length value cannot fit in 2 bytes
+  // Check if the payload length exceeds 125 bytes and less than 65535 bytes
+  if (payloadByteLength > 125) {
+    // The length cannot fit in 1 byte, so increase the offset to accommodate 2 bytes
+    payloadBytesOffset += 2;
+  } else if (payloadByteLength > 65535) {
+    // The length value cannot fit in 2 bytes, so increase the offset to accommodate 8 bytes
     payloadBytesOffset += 8;
   }
-  // Check if the payload length exceeds 125 bytes
-  else if (payloadByteLength > 125) {
-    payloadBytesOffset += 2;
-  }
+  
 
-  // Create a buffer with the appropriate size
+// this is common for all condition
+  // Create a buffer by adding both payloadbytesoffset and payloadlength
   const buffer = Buffer.alloc(payloadBytesOffset + payloadByteLength);
-
-  // Write the first byte of the frame
+  // Write the first byte of the frame (it contain opcodes etc..)
   buffer.writeUInt8(0b10000001, 0);
-  // Write the second byte - actual payload size (if <= 125 bytes) or 126, or 127
-  buffer[1] = payloadByteLength
 
-  // Check if the payload length is 126
-  if (payloadByteLength === 126) {
-    // Write the actual payload length as a 16-bit unsigned integer
+
+  if (payloadByteLength <= 125) {
+    //  we setup 2nd postion in the buffer to start the payload (our default offset=2)
+    buffer.writeUint8(payloadByteLength, 1);
+  } else if (payloadByteLength === 126) {
+    // Write the actual payload length as a 16-bit unsigned integer in the 2nd index(3rd postion) of the buffer
     buffer.writeUInt16BE(payloadByteLength, 2);
-  }
-  // Check if the payload length is 127
-  else if (payloadByteLength === 127) {
+  } else if (payloadByteLength === 127) {
     // Write the actual payload length as a 64-bit unsigned integer
     buffer.writeBigUInt64BE(BigInt(payloadByteLength), 2);
   }
@@ -41,6 +38,5 @@ module.exports.createFrame = (data) => {
   // Write the payload into the buffer at the appropriate offset
   buffer.write(payload, payloadBytesOffset);
 
-  // Return the frame buffer
   return buffer;
 };
